06d684d857229cb738e41afe508b0f07765ef072
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-03 13:14:53 +0100

    Fertigstellung Twelv

--- uebung01/twelv.rb
+++ uebung01/twelv.rb
@@ -8,6 +8,8 @@ class Twelv
 	def initialize()
 		@taken_numbers = []
 		@left_numbers = *(1..12)
+		take_number
+		puts "guess_higher or guess_lower"
 	end
 	
 	# Ziehe eine Number
@@ -18,7 +20,44 @@ class Twelv
 		number = @left_numbers[Random.rand(@left_numbers.length)]
 		@taken_numbers.push(number)
 		@left_numbers -= [number]
+		puts to_s
 		return number
 	end
+	
+	# Raten
+	def guess(relation)
+		if take_number() == nil
+			puts "Game has ended. You won \n"
+			puts "Initialize new game...\n"
+			initialize()
+			return
+		end
+		
+		if (@taken_numbers[-1] <=> @taken_numbers[-2]) == relation
+ 			puts "Right guess! \n"
+		else	
+			puts "Game Over! \n"
+			puts "Initialize new game...\n"
+			initialize()
+		end
+	end
+	
+	# Ergebnis zeigen
+	def to_s
+		return @taken_numbers.join(", ")
+	end
+	
+	# Nächste Zahl ist höher
+	def guess_higher() 
+		guess(1)
+	end
+	
+	# Nächste Zahl ist tiefer
+	def guess_lower()
+		guess(-1)
+	end
+	
+	private :take_number
+	private :guess
 
 end
