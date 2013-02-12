f8b4e371dd93f22d227f98993827f56315f169f3
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-12 10:59:06 +0100

    Test-refactoring
    first argument of assert_equal is the expected result, second one is the actual result

--- uebung01-02/tc_twelv.rb
+++ uebung01-02/tc_twelv.rb
@@ -10,7 +10,7 @@ class TestTwelv < Test::Unit::TestCase
   def test_lower
     @twelv.taken_numbers = *(2..12)
     @twelv.left_numbers = [1]
-    assert_equal(@twelv.guess_lower,true)
+    assert_equal(true,@twelv.guess_lower)
     assert_equal(11,@twelv.left_numbers.length)
     assert_equal(1,@twelv.taken_numbers.length)
   end
@@ -18,7 +18,7 @@ class TestTwelv < Test::Unit::TestCase
   def test_lower
     @twelv.taken_numbers = *(1..6)
     @twelv.left_numbers = [12]
-    assert_equal(@twelv.guess_higher,true)
+    assert_equal(true,@twelv.guess_higher)
     assert_equal(0,@twelv.left_numbers.length)
     assert_equal(7,@twelv.taken_numbers.length)
   end
@@ -26,7 +26,7 @@ class TestTwelv < Test::Unit::TestCase
   def test_lower_with_left_numbers
     @twelv.taken_numbers = *(1..6)
     @twelv.left_numbers = [10,11,12]
-    assert_equal(@twelv.guess_higher,true)
+    assert_equal(true,@twelv.guess_higher)
     assert_equal(2,@twelv.left_numbers.length)
     assert_equal(7,@twelv.taken_numbers.length)
   end
@@ -34,7 +34,7 @@ class TestTwelv < Test::Unit::TestCase
   def test_higher_false
     @twelv.taken_numbers = *(3..12)
     @twelv.left_numbers = [1, 2]
-    assert_equal(@twelv.guess_higher,false)
+    assert_equal(false,@twelv.guess_higher)
     assert_equal(11,@twelv.left_numbers.length)
     assert_equal(1,@twelv.taken_numbers.length)
   end
@@ -42,7 +42,7 @@ class TestTwelv < Test::Unit::TestCase
   def test_lower_false
     @twelv.taken_numbers = *(1..11)
     @twelv.left_numbers = [12]
-    assert_equal(@twelv.guess_lower,false)
+    assert_equal(false,@twelv.guess_lower)
     assert_equal(11,@twelv.left_numbers.length)
     assert_equal(1,@twelv.taken_numbers.length)
   end
