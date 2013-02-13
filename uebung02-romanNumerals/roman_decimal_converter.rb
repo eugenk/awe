4e54e9948d50de8542ef6c1b5bb4074ee6bd1ab0
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-13 12:38:24 +0100

    Refacroting: renamed variables

--- uebung02-romanNumerals/roman_decimal_converter.rb
+++ uebung02-romanNumerals/roman_decimal_converter.rb
@@ -14,47 +14,47 @@ class RomanDecimalConverter
 
   def int_to_roman(num)
     raise NonPositiveNumberError, "Argument not positive: #{num}" unless num > 0
-    r = ''
+    result = ''
     @sym_val_map.each do |roman, decimal|
       (num / decimal).times do
-        r << roman
+        result << roman
       end
       num %= decimal
     end
-    r
+    result
   end
 
-  def roman_to_int(r)
-    r = r.upcase
-    raise UnknownRomanSymbolError, "Unknown roman symbol in: #{r}" if contains_unknown_symbol?(r)
-    raise BadSymbolOrderError, "Bad symbol order in: #{r}" if has_bad_symbol_order?(r)
-    roman_to_int_worker(r)
+  def roman_to_int(roman_num)
+    roman_num = roman_num.upcase
+    raise UnknownRomanSymbolError, "Unknown roman symbol in: #{roman_num}" if contains_unknown_symbol?(roman_num)
+    raise BadSymbolOrderError, "Bad symbol order in: #{roman_num}" if has_bad_symbol_order?(roman_num)
+    roman_to_int_worker(roman_num)
   end
 
-  def roman_to_int_worker(r)
-    if r.empty?
+  def roman_to_int_worker(roman_num)
+    if roman_num.empty?
       return 0
     end
 
     @sym_val_map.each do |roman, decimal|
-      pos = r.index(roman)
+      pos = roman_num.index(roman)
       next if pos == nil
       if pos == 0
-        return decimal + roman_to_int_worker(r[pos+1..-1])
+        return decimal + roman_to_int_worker(roman_num[pos+1..-1])
       elsif pos > 0
-        return decimal - roman_to_int_worker(r[0..pos-1]) + roman_to_int_worker(r[pos+1..-1])
+        return decimal - roman_to_int_worker(roman_num[0..pos-1]) + roman_to_int_worker(roman_num[pos+1..-1])
       end
     end
     0
   end
 
-  def contains_unknown_symbol?(r)
-    !r.delete('MDCLXVI').empty?
+  def contains_unknown_symbol?(roman_num)
+    !roman_num.delete('MDCLXVI').empty?
   end
 
-  def has_bad_symbol_order?(r)
+  def has_bad_symbol_order?(roman_num)
     %w"IL IC ID IM VX VL VC VD VM XD XM LC LD LM DM".each do |s|
-      if r.index(s)
+      if roman_num.index(s)
         return true
       end
     end
