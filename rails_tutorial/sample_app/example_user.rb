2a9b3d42198510f3b24c146c508f534cbb6330da
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2013-02-15 15:53:24 +0100

    Add Excercise 4

--- rails_tutorial/sample_app/example_user.rb
+++ rails_tutorial/sample_app/example_user.rb
@@ -9,4 +9,22 @@ class User
   def formatted_email
     "#{@name} <#{@email}>"
   end
+  
 end
+
+class String
+  def shuffle
+    self.split('').shuffle.join('')
+  end
+  
+  def string_shuffle(s)
+    s.shuffle
+  end
+end
+
+
+person1 = { first: "Hans", last: "Newton" }
+person2 = { first: "Jalyna", last: "Schroeder" }
+person3 = { first: "Eugen", last: "Kuksa" }
+
+params = { father: person1, mother: person2, child: person3 }
