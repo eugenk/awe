14814255d4723f169166914cc090b9baa861db14
jalyna <jalyna@informatik.uni-bremen.de>
2013-02-15 12:50:57 +0100

    Improve Static Pages

--- rails_tutorial/sample_app/spec/requests/static_pages_spec.rb
+++ rails_tutorial/sample_app/spec/requests/static_pages_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
   	it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       page.should have_content('Sample App')
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',:text => "#{base_title} | Home")
     end
   end
 
@@ -14,12 +20,31 @@ describe "StaticPages" do
       visit '/static_pages/help'
       page.should have_content('Help')
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title',:text => "#{base_title} | Help")
+    end
   end
 
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title',:text => "#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact')
+    end
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',:text => "#{base_title} | Contact")
     end
   end
 
